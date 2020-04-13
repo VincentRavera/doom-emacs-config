986bc23618c72019ad9f34496a62c7c59e9a03fc
Vince <ravera.vincent@gmail.com>
2020-04-13 15:11:21 +0200

    Changed theme

--- config.el
+++ config.el
@@ -18,12 +18,14 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+;; (setq doom-font (font-spec :family "monospace" :size 14))
+(add-to-list 'default-frame-alist
+                       '(font . "DejaVu Sans Mono-14"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-gruvbox)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/workspace/wiki/VRA-Notes/")
@@ -49,8 +51,6 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
-(add-to-list 'default-frame-alist
-                       '(font . "DejaVu Sans Mono-14"))
 ;; install dependencies
 ;; sudo add-apt-repository ppa:x4121/ripgrep
 ;; sudo apt-get install ripgrep
