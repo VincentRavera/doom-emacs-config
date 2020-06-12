a63ce56c4101d50d7f9b2c075e662fde2bf4848e
Vincent Ravera <vincent.ravera@atos.net>
2020-06-12 20:10:47 +0200

    Smarter eshell find-file

--- config.el
+++ config.el
@@ -157,8 +157,18 @@
   (add-to-list 'eshell-visual-commands "top")
   (add-to-list 'eshell-visual-options '("git" "--help"))
   (add-to-list 'eshell-visual-subcommands '("git" "log" "diff" "show"))
-  (setq eshell-destroy-buffer-when-process-dies nil))
+  (setq eshell-destroy-buffer-when-process-dies nil)
+
+  (defun me/remote-find-file (input-file-name)
+    "open remotly the file you want"
+    (let ((prefix-tramp (file-remote-p (eshell/pwd))))
+      (find-file (concat prefix-tramp input-file-name)))))
+
+;; get TRAMP prefix
+;; (tramp-handle-file-remote-p $PWD) => /sudo:root@localhost:
+
+
 
 ;; undo
-  (after! undo-tree
-    (setq undo-tree-auto-save-history nil))
+(after! undo-tree
+  (setq undo-tree-auto-save-history nil))
