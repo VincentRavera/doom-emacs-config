5f1c20df2fec8256b84eee30f64f7742bcec6d04
Vincent RAVERA <vravera@ncelrnd2886.nce.amadeus.net>
2024-01-08 12:14:20 +0100

    Add Edifacts requests

--- edifact/od-edi.el
+++ edifact/od-edi.el
@@ -6,6 +6,8 @@
 (require 'ob)
 (require 'seq)
 (require 's)
+(require 'dash)
+(require 'request)
 (require 'edi-mode)
 
 (defvar org-babel-default-header-args:edi
@@ -23,8 +25,46 @@ The evaluation returns the formmatted edi.")
 
 (defun org-babel-execute:edi (body params)
   "Execute edi BODY but PARAMS are ignored."
-  (message (format "ediparams: %s" params))
-  (with-temp-buffer
-    (insert body)
-    (edi-edi-to-readable)
-    (buffer-string)))
+  (setq vra-debug-var
+        (--reduce-from
+         (if (eq :var (car it))
+           (cons (cdr it) acc)
+           acc)
+         '()
+         params))
+  (setq vra-debug params)
+  ;; TODO Add Proxy overide config
+  (let* ((vars (--reduce-from
+                (if (eq :var (car it))
+                    (cons (cdr it) acc)
+                  acc)
+                '()
+                params))
+         (edi-host (cdr (assoc 'host vars)))
+         (edi-route (cdr (assoc 'route vars)))
+         (edi-url (format "https://%s%s" edi-host edi-route))
+         (edifact (edi-edi-ensure-not-readable body))
+         (request-curl-options '("-k")))
+    (message (format "XXX %s -> %s" edi-url edifact))
+    (with-temp-buffer
+      (insert
+       (request-response-data
+        (request edi-url
+          :type "POST"
+          :sync t
+          :headers '(("Content-Type" . "application/json")
+                     ("accept" . "application/json"))
+          :data edifact
+          ;; :success (cl-function
+          ;;           (lambda (&key data &allow-other-keys)
+          ;;             (message "Got: %s" data)))
+          )))
+      (edi-edi-to-readable)
+      (buffer-string))))
+;; (with-temp-buffer
+;;   (insert ())
+;;   (edi-edi-to-readable)
+;;   (buffer-string)))))))
+
+(defun edi-edi-ensure-not-readable (edi)
+  (s-join "" (s-split "\n" edi 'omit-nulls)))
