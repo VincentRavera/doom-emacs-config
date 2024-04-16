e5a98f3e2ab377fb6b8f4844fc4e4030fb4cdad2
Vincent RAVERA <ravera.vincent@gmail.com>
2024-04-16 14:39:45 +0200

    Update forced/evil

--- utils/forced-evil.el
+++ utils/forced-evil.el
@@ -1,74 +1,63 @@
 ;;; utils/main.el -*- lexical-binding: t; -*-
 
 (require 'transient)
+;; Examples
+;; https://magit.vc/manual/0.1.0/transient.html#Invoking-Transients
+;; https://flonic.gitlab.io/org-blog/blog/emacs-transient-tutorial/index.html
 
-(define-suffix-command me/forced-evil:up ()
-   :description "Up"
-   :transient t
-   (interactive)
-   (evil-previous-line)
-  )
 
-(define-suffix-command me/forced-evil:down ()
-   :description "Down"
-   :transient t
-   (interactive)
-   (evil-next-line)
-  )
-
-(define-suffix-command me/forced-evil:left ()
-   :description "Left"
-   :transient t
-   (interactive)
-   (evil-backward-char)
-  )
-
-(define-suffix-command me/forced-evil:right ()
-   :description "right"
-   :transient t
-   (interactive)
-   (evil-forward-char)
-  )
-
-(define-suffix-command me/forced-evil:scroll-down ()
-   :description "Page Down"
-   :transient t
-   (interactive)
-   (scroll-up-command)
-  )
-
-(define-suffix-command me/forced-evil:scroll-up ()
-   :description "Page Up"
-   :transient t
-   (interactive)
-   (scroll-down-command)
-  )
-
-(define-suffix-command me/forced-evil:gg ()
-   :description "Begining"
-   :transient t
-   (interactive)
-   (evil-goto-first-line)
-  )
-
-(define-suffix-command me/forced-evil:G ()
-   :description "End"
-   :transient t
-   (interactive)
-   (evil-goto-line)
-  )
-
-(define-transient-command me/forced-evil ()
+(transient-define-prefix me/forced-evil ()
   "Test Transient Title"
   ["Move"
-   ("k" me/forced-evil:up)
-   ("j" me/forced-evil:down)
-   ("h" me/forced-evil:left)
-   ("l" me/forced-evil:right)]
+   ("k" "Up"    me/forced-evil:up)
+   ("j" "Down"  me/forced-evil:down)
+   ("h" "Left"  me/forced-evil:left)
+   ("l" "Right" me/forced-evil:right)]
   ["Jump"
-   ("C-u" me/forced-evil:scroll-up)
-   ("C-d" me/forced-evil:scroll-down)
-   ("g" me/forced-evil:gg)
-   ("G" me/forced-evil:G)
+   ("C-u" "Up"          me/forced-evil:scroll-up)
+   ("C-d" "Down"        me/forced-evil:scroll-down)
+   ("g" "BOF"           me/forced-evil:gg)
+   ("G" "EOF"           me/forced-evil:G)
    ]
   )
+
+(defun me/forced-evil:up ()
+  (interactive)
+  (evil-previous-line)
+  (me/forced-evil))
+
+(defun me/forced-evil:down ()
+  (interactive)
+  (evil-next-line)
+  (me/forced-evil))
+
+(defun me/forced-evil:left ()
+  (interactive)
+  (evil-backward-char)
+  (me/forced-evil))
+
+(defun me/forced-evil:right ()
+  (interactive)
+  (evil-forward-char)
+  (me/forced-evil))
+
+(defun me/forced-evil:scroll-down ()
+  (interactive)
+  (scroll-up-command)
+  (evil-scroll-up 40)
+  (me/forced-evil))
+
+(defun me/forced-evil:scroll-up ()
+  (interactive)
+  (evil-scroll-down 40)
+  (me/forced-evil))
+
+(defun me/forced-evil:gg ()
+  (interactive)
+  (evil-goto-first-line)
+  (me/forced-evil))
+
+(defun me/forced-evil:G ()
+  (interactive)
+  (evil-goto-line)
+  (me/forced-evil))
